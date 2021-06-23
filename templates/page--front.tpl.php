6bd5cb8e69e25ae32fbbf67ae4e829dad7cf4459
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-06-23 13:01:54 +0100

    Adding sm classes to the tpl.php

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -171,7 +171,7 @@ if(!empty($file_fid)) {
   <section class="preface front__flex-margin">
     <div class="row row-eq-height">
       <?php if ($page['preface']): ?>
-      <div class="<?php if (empty($page['preface_side'])) { print 'col-sm-12 fullscreen'; } else { print 'col-sm-9 fullscreen'; } ?>">
+      <div class="<?php if (empty($page['preface_side'])) { print 'col-sm-12 fullscreen'; } else { print 'col-md-9 col-sm-12 fullscreen'; } ?>">
   	     <div class="preface-main full-height">
   			      <?php print render($page['preface']);?>
   	     </div>
@@ -179,7 +179,7 @@ if(!empty($file_fid)) {
   	  <?php endif; ?>
 
       <?php if (!empty($page['preface_side'])): ?>
-        <aside class="col-sm-3 col-md-3 fullscreen">
+        <aside class="col-sm-12 col-md-3 fullscreen">
           <?php print render($page['preface_side']); ?>
         </aside>  <!-- /#sidebar-first -->
       <?php endif; ?>
