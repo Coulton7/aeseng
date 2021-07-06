2bcb72c596761e26a52a69273ea1c7a0abdd93e0
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-06 16:11:35 +0100

    Updating the script again hopefully will fire this time around

--- js/home-carousel-hider.js
+++ js/home-carousel-hider.js
@@ -1,23 +1,25 @@
 (function($) {
-  $('#carousel-fade').on('slid.bs.carousel', function() {
-    var carouselData = $(this).data('bs.carousel');
-    var currentIndex = carouselData.getItemIndex(carouselData.$element.find('.item.active'));
-    var total = carouselData.$items.length;
-    var slide = (currentIndex + 1);
-    if (slide === 1) {
-      if ($('.sus-media-carousel').hasClass('display-none')) {
-        $('.sus-media-carousel').removeClass('display-none');
+  $(document).ready(function() {
+    $('#carousel-fade').on('slid.bs.carousel', function() {
+      var carouselData = $(this).data('bs.carousel');
+      var currentIndex = carouselData.getItemIndex(carouselData.$element.find('.item.active'));
+      var total = carouselData.$items.length;
+      var slide = (currentIndex + 1);
+      if (slide === 1) {
+        if ($('.sus-media-carousel').hasClass('display-none')) {
+          $('.sus-media-carousel').removeClass('display-none');
+        }
+        if (!$('.why-media-carousel').hasClass('display-none')) {
+          $('.why-media-carousel').addClass('display-none');
+        }
+      } else if (slide === 2) {
+        if ($('.why-media-carousel').hasClass('display-none')) {
+          $('.why-media-carousel').removeClass('display-none');
+        }
+        if (!$('.sus-media-carousel').hasClass('display-none')) {
+          $('.sus-media-carousel').addClass('display-none');
+        }
       }
-      if (!$('.why-media-carousel').hasClass('display-none')) {
-        $('.why-media-carousel').addClass('display-none');
-      }
-    } else if (slide === 2) {
-      if ($('.why-media-carousel').hasClass('display-none')) {
-        $('.why-media-carousel').removeClass('display-none');
-      }
-      if (!$('.sus-media-carousel').hasClass('display-none')) {
-        $('.sus-media-carousel').addClass('display-none');
-      }
-    }
+    });
   });
 })(jQuery);
