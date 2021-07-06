912600b33c88470a47584ed5910f878c26d675a7
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-06 12:34:49 +0100

    Adding 2 new js to page tpl 1 to remove from block and other for testing

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -91,7 +91,8 @@ if(!empty($file_fid)) {
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/brands.css" integrity="sha384-rf1bqOAj3+pw6NqYrtaE1/4Se2NBwkIfeYbsFdtiR6TQz0acWiwJbv1IM/Nt/ite" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/fontawesome.css" integrity="sha384-1rquJLNOM3ijoueaaeS5m+McXPJCGdr5HcA03/VHXxcp2kX2sUrQDmFc3jR5i/C7" crossorigin="anonymous">
 <script src="/sites/all/themes/aesbs337/js/slider-options.js"></script>
-
+<script src="/sites/all/themes/aesbs337/js/carousel-options.js"></script>
+<script src="/sites/all/themes/aeseng/js/home-carousel-hider.js"></script>
 
 <div class="container-fluid bannercontainer">
 	<div class="row bannerimage">
