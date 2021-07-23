d9f6068771db14556c99f4ffcd14caa8e162a137
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-23 11:41:21 +0100

    Adjusted the js for the shadow-dropmenu

--- js/shadow-drop-mobile.js
+++ js/shadow-drop-mobile.js
@@ -1,25 +1,9 @@
 (function($) {
   $(document).ready(function() {
-    $("<select />").appendTo(".shadow-box-tabs");
-
-    // Create default option "Go to..."
-    $("<option />", {
-       "selected": "selected",
-       "value"   : "",
-       "text"    : "Go to..."
-    }).appendTo(".shadow-drop");
-
-    // Populate dropdown with menu items
-    $(".shadow-box-tabs .nav a").each(function() {
-     var el = $(this);
-     $("<option />", {
-         "value"   : el.attr("href"),
-         "text"    : el.text()
-     }).appendTo(".shadow-drop");
-    });
-
-    $(".shadow-drop").change(function() {
-      window.location = $(this).find("option:selected").val();
-    });
-  });
+    $(function(){
+       $(".dropdown-menu").on('click', 'li a', function(){
+         $(".shadow-drop-btn:first-child").text($(this).text());
+         $(".shadow-drop-btn:first-child").val($(this).text());
+      });
+   });
 })(jQuery);
