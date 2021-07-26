463791fca5e382f052583541b1e9361d3c01d54f
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-07-26 09:37:09 +0100

    Added js to try and have it so the text + icon will move down on scroll

--- js/shadow-drop-mobile.js
+++ js/shadow-drop-mobile.js
@@ -3,4 +3,12 @@
     $(".shadow-drop:first-child").text($(this).text());
     $(".shadow-drop:first-child").val($(this).text());
  });
+
+ $(function() {
+  $(document).scroll(function(){
+    var $container = $(".net-zero-container", ".mobile-carousel-caption");
+    $container.toggleClass('scrolled', $(this).scrollTop() > $container.height());
+  });
+});
+
 })(jQuery);
