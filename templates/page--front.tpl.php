ae24ca60d915dd98f17a66e7b4f5aabf35cb480a
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2021-08-25 09:17:14 +0100

    Slight adjustment to the tpl so search fits better

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -160,9 +160,9 @@ if(!empty($file_fid)) {
 	<?php if ($page['navigation_col']): ?>
 		<div class="navigation_col">
 			<div class="action-menu text-center">
-				<div class="col-sm-2 col-xs-0 fullscreen">
+				<div class="col-md-2 col-sm-0 col-xs-0 fullscreen">
 				</div>
-				<div class="col-sm-10 col-sm-offset-2 col-xs-12 no-padding">
+				<div class="col-md-10 col-md-offset-2 col-sm-12 col-xs-12 no-padding">
 				<?php print render($page['navigation_col']);?>
 			</div>
 			</div>
